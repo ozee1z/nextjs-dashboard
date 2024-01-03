fc58ae15f62aefbfcb2c0c98006d66bfc7392e03
ozee1z <aanyehschlink@yahoo.com>
2024-01-03 14:31:41 +0000

    made changes to font.ts

--- next.config.js
+++ next.config.js
@@ -2,12 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-
-const withFonts = require('next-fonts');
-
-module.exports = withFonts({
-  // Other configurations...
-  webpack(config, options) {
-    return config;
-  },
-});
